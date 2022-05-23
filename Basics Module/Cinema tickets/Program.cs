5b709577a6fffe2e66ceebda5aa9f6d266682f3b
Plamen Velikov <plamenrv5@gmail.com>
2022-05-23 22:44:12 +0300

    Commiting file from Basics Module.

---
+++ Basics Module/Cinema tickets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace cinema_tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int counter = 0;
+            string numToPrint = string.Empty;
+
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 1; b <= 9; b++)
+                {
+                    for (int c = 1; c <= 9; c++)
+                    {
+                        for (int d = 1; d <= 9; d++)
+                        {
+                            if (a < b && c > d)
+                            {
+                                int x = a * b + c * d;
+
+                                if (x == m)
+                                {
+                                    Console.Write($"{a}{b}{c}{d} ");
+                                    counter++;
+
+                                    if (counter == 4)
+                                    {
+                                        numToPrint = Convert.ToString(a) + Convert.ToString(b) + Convert.ToString(c) + Convert.ToString(d);
+                                    }
+                                    
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            if (counter >= 4)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Password: {numToPrint}");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"No!");
+            }
+        }
+    }
+}
