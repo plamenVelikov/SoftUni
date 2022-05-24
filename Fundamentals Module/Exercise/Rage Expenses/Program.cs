4cde6736266b8a0d721767c8a8a65cdac8d04bb5
Plamen Velikov <plamenrv5@gmail.com>
2022-05-24 19:16:51 +0300

    Creating a new project.

---
+++ Fundamentals Module/Exercise/Rage Expenses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
